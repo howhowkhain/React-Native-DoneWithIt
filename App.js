fdaf181268a29d5807babe1eed8ec4df5409b84d
Alexander Vasil <alexander.nic@sediu.dedeman>
2023-11-16 15:06:23 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
+  console.log('aaa');
+  console.log(require('./assets/icon.png'))
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello React Native</Text>
+      <Image source={require('./assets/icon.png')}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +17,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
+    
   },
+  image: {
+    width: 200,
+    height: 200,
+  }
 });
