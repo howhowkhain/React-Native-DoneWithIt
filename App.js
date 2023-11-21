fb157801ba753fb5420a27b8974aba4128e9aef2
howhowkhain <howhowkhain@yahoo.co.uk>
2023-11-21 22:12:46 +0200

    Layout-Icons

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 import AppText from "./app/components/AppText";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function App() {
   return (
     // <WelcomeScreen/>
     // <ViewImageScreen/>
     <View style={styles.container}>
-      <AppText>My React Native App</AppText>
+      {/* <AppText>My React Native App</AppText> */}
+      <MaterialCommunityIcons name="email" size={60} color={"dodgerblue"} />
     </View>
   );
 }
