79df9b21412432bc1517f3885c157e77b91081fa
howhowkhain <howhowkhain@yahoo.co.uk>
2023-11-27 16:10:43 +0200

    Lists-Exctracting the Screen Component

--- App.js
+++ App.js
@@ -16,11 +16,13 @@ import AppButton from "./app/components/AppButton";
 import AppCard from "./app/components/AppCard";
 import colors from "./app/config/colors";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
   return (
+    <MessagesScreen />
     // <WelcomeScreen />
-    <ViewImageScreen />
+    // <ViewImageScreen />
     // <ListingDetailsScreen />
     // <View style={styles.container}>
     //   {/* <AppText>My React Native App</AppText> */}
