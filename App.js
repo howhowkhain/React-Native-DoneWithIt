2fbbc1a2d54c053c3756512a3f1dc5fee62b1576
howhowkhain <howhowkhain@yahoo.co.uk>
2023-12-01 12:06:47 +0200

    Building a Pretty TextInput

--- App.js
+++ App.js
@@ -1,66 +1,25 @@
-import {
-  StyleSheet,
-  View,
-  SafeAreaView,
-  Text,
-  Button,
-  Platform,
-} from "react-native";
+import { StyleSheet, TextInput } from "react-native";
 
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import AppText from "./app/components/AppText/AppText";
-import AppButton from "./app/components/AppButton";
-import AppCard from "./app/components/AppCard";
-import colors from "./app/config/colors";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
 import Screen from "./app/components/Screen";
-import MyAccountScreen from "./app/screens/MyAccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
+import colors from "./app/config/colors";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
   return (
     <Screen style={styles.screen}>
-      <GestureHandlerRootView>
-        {/* <MessagesScreen /> */}
-        {/* <MyAccountScreen /> */}
-        <ListingsScreen />
-      </GestureHandlerRootView>
+      <AppTextInput icon={"email"} placeholder={"First Name"} />
+      {/* <TextInput
+        placeholder="First Name"
+        maxLength={5}
+        keyboardType="numeric"
+      ></TextInput> */}
     </Screen>
-    // <WelcomeScreen />
-    // <ViewImageScreen />
-    // <ListingDetailsScreen />
-    // <View style={styles.container}>
-    //   {/* <AppText>My React Native App</AppText> */}
-    //   {/* <MaterialCommunityIcons name="email" size={60} color={"dodgerblue"} /> */}
-    //   {/* <AppButton
-    //     title="Login"
-    //     onPress={() => console.log("Pressed")}
-    //   /> */}
-    //   {/* <AppCard
-    //     title="Red Jacket for sale!"
-    //     subTitle="$100"
-    //     image={require("./app/assets/jacket.jpg")}
-    //   /> */}
-    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    backgroundColor: colors.light,
+    backgroundColor: colors.white,
     padding: 20,
-  },
-  container: {
-    // flex: 1,
-    // justifyContent: "center",
-    // alignItems: "center",
-    backgroundColor: colors.light,
-    padding: 20,
-    paddingTop: 100,
   },
 });
