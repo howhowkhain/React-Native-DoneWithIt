8c7acc4077c585586b212c5d56af1ddbed8386cf
howhowkhain <howhowkhain@yahoo.co.uk>
2023-12-04 22:58:22 +0200

    Forms-Organizing Components

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { Image, StyleSheet } from "react-native";
-import { Formik } from "formik";
 import * as Yup from "yup";
-import AppButton from "../components/AppButton";
-import AppFormField from "../components/AppFormField";
-import SubmitButton from "../components/SubmitButton";
-import AppForm from "../components/AppForm";
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
