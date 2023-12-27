d1423cb81ac8a45c25997c44f30d050d9c6f898f
howhowkhain <howhowkhain@yahoo.co.uk>
2023-12-27 20:35:53 +0200

    Passing Parameters to Routes

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {
   Image,
   StyleSheet,
   Switch,
+  Text,
   TextInput,
 } from "react-native";
 
@@ -21,33 +22,77 @@ import MessagesScreen from "./app/screens/MessagesScreen";
 import * as ImagePicker from "expo-image-picker";
 import ImageInput from "./app/components/ImageInput";
 import ImageInputList from "./app/components/ImageInputList";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+
+const Link = () => {
+  const navigation = useNavigation();
+  return (
+    <Button title="Click" onPress={() => navigation.navigate("TweetDetails")} />
+  );
+};
+
+const Tweets = ({ navigation }) => {
+  return (
+    <Screen>
+      <Text>Tweets</Text>
+      <Button
+        title="View Tweet"
+        onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
+      ></Button>
+      {/* <Link /> */}
+    </Screen>
+  );
+};
+
+const TweetDetails = ({ route }) => {
+  return (
+    <Screen>
+      <Text>Tweet Details {route.params.id}</Text>
+    </Screen>
+  );
+};
+
+const StackNavigator = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <Stack.Navigator initialRouteName="Tweets">
+      <Stack.Screen name="Tweet" component={Tweets} />
+      <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   const [category, setCategory] = useState(null);
   const [imageUris, setImageUris] = useState([]);
 
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
+  // const handleAdd = (uri) => {
+  //   setImageUris([...imageUris, uri]);
+  // };
 
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-  };
+  // const handleRemove = (uri) => {
+  //   setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
+  // };
 
   return (
-    <Screen style={styles.screen}>
-      {/* <ImageInputList
-        imageUris={imageUris}
-        onAddImage={handleAdd}
-        onRemoveImage={handleRemove}
-      /> */}
-      {/* <LoginScreen /> */}
-      {/* <RegisterScreen /> */}
-      <ListingEditScreen />
-      {/* <GestureHandlerRootView>
-        <MessagesScreen />
-      </GestureHandlerRootView> */}
-    </Screen>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+    // <Screen style={styles.screen}>
+    //   {/* <ImageInputList
+    //     imageUris={imageUris}
+    //     onAddImage={handleAdd}
+    //     onRemoveImage={handleRemove}
+    //   /> */}
+    //   {/* <LoginScreen /> */}
+    //   {/* <RegisterScreen /> */}
+    //   {/* <ListingEditScreen /> */}
+    //   {/* <GestureHandlerRootView>
+    //     <MessagesScreen />
+    //   </GestureHandlerRootView> */}
+    // </Screen>
   );
 }
 
